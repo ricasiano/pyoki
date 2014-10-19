530e69d6df9ac1faf3741fb7f3e1ffc66142c1fc
rai <ricasiano@gmail.com>
2014-10-20 01:45:50 +0800

    missing file for db handling

---
+++ DB.py
@@ -0,0 +1,18 @@
+import sqlite3 as sql
+con = sql.connect('/home/rai/pyoki/embeddedapp/pyoki/Pyoki.db')
+
+class DBConnect:
+ def getOne(self, query, id):
+   with con:
+     con.row_factory = sql.Row
+     cur = con.cursor()    
+     cur.execute(query, (id,))
+     row = cur.fetchone()
+     return row
+ def getAll(self, query):
+   with con:
+     con.row_factory = sql.Row
+     cur = con.cursor()    
+     cur.execute(query)
+     rows = cur.fetchall()
+     return rows
